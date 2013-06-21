78cb1a97210383b520b7c88b1611837d3df85ff2
drq <yong.qu@yottaa.com>
2013-06-21 06:06:01 -0400

    Added consumer sample. Cleaned up provision and deprovision code.

--- app.rb
+++ app.rb
@@ -1,14 +1,29 @@
 require 'bundler'
+require 'net/http'
+require 'net/https'
+require 'uri'
+require 'json'
+
 Bundler.require
 
 STDOUT.sync = true
 
+# utility class for yottaa custom header
+class CaseSensitiveString < String
+  def downcase
+    self
+  end
+
+  def capitalize
+    self
+  end
+end
+
 class App < Sinatra::Base
   use Rack::Session::Cookie, secret: ENV['SSO_SALT']
 
-  @@resources = []
-
-  Resource = Class.new(OpenStruct)
+  YOTTAA_CUSTOM_HEADER = {CaseSensitiveString.new("YOTTAA-API-KEY") =>'39e8b9b0b1000130d279123138106137'}
+  YOTTAA_API_URL_ROOT = "https://api-dev.yottaa.com/partners/51b0cd1bebe2bb3069000f0d"
 
   helpers do
     def protected!
@@ -40,7 +55,32 @@ class App < Sinatra::Base
     end
 
     def get_resource
-      @@resources.find {|u| u.id == params[:id].to_i } or halt 404, 'resource not found'
+      # locate Yottaa account by site id
+      site_id = params[:id]
+      user_id = ENV["YOTTAA_USER_ID"]
+      if ! user_id.nil?
+        uri = URI.parse(YOTTAA_API_URL_ROOT + "/accounts/" + user_id + "/sites/" + site_id)
+        https = https_connection(uri)
+        req = Net::HTTP::Get.new(uri, YOTTAA_CUSTOM_HEADER)
+        https.set_debug_output($stdout)
+
+        res = https.request(req)
+        result = JSON.parse(res.body)
+        if result.has_key? 'id'
+          return result
+        else
+          halt 404, 'resource not found'
+        end
+      else
+        halt 404, 'resource not found'
+      end
+    end
+
+    def https_connection (uri)
+      https = Net::HTTP.new(uri.host, uri.port)
+      https.use_ssl = true
+      https.verify_mode = OpenSSL::SSL::VERIFY_NONE
+      return https
     end
   end
 
@@ -54,6 +94,9 @@ class App < Sinatra::Base
   end
 
   def sso
+    STDOUT.puts ENV['SSO_SALT']
+    STDOUT.puts params[:id]
+    STDOUT.puts params[:timestamp]
     pre_token = params[:id] + ':' + ENV['SSO_SALT'] + ':' + params[:timestamp]
     token = Digest::SHA1.hexdigest(pre_token).to_s
     halt 403 if token != params[:token]
@@ -83,30 +126,67 @@ class App < Sinatra::Base
   post '/heroku/resources' do
     show_request
     protected!
-    if json_body['region'] != 'amazon-web-services::us-east-1'
+    options = json_body.fetch('options', {})
+    first_name = options['first_name'].nil? ? "" : options['first_name']
+    last_name = options['last_name'].nil? ? "" : options['last_name']
+    phone = options['phone'].nil? ? "" : options['phone']
+    email = options['email'].nil? ? json_body.fetch('heroku_id') : options['email']
+    # TODO: figure out the default custom domain name?
+    site = options['site'].nil? ? "" : options['site']
+    plan = json_body.fetch("plan", "free")
+    if !email.empty? && !site.empty?
+      STDOUT.puts "Creating a new Yottaa account"
+
+      uri = URI.parse(YOTTAA_API_URL_ROOT + "/accounts")
+      https = https_connection(uri)
+      req = Net::HTTP::Post.new(uri, YOTTAA_CUSTOM_HEADER)
+      req.set_form_data({"first_name" => first_name, "last_name" => last_name, "email" => email, "site" => site, "phone" => phone, "plan" => plan})
+      https.set_debug_output($stdout)
+
+      res = https.request(req)
+      result = JSON.parse(res.body)
+
+      if !result.has_key? 'error'
+        status 201
+        body({
+                 :id => result["site_id"],
+                 :config => {"YOTTAA_SITE_ID" => result["site_id"], "YOTTAA_USER_ID" => result["user_id"], "YOTTAA_API_KEY" => result["api_key"]},
+                 :message => 'Dear ' + first_name + ' ' + last_name +', your Yottaa account is now provisioned!'
+             }.to_json)
+      else
+        status 422
+        body(result['error'].to_json)
+      end
+    else
       status 422
-      body({:error => 'Region is not supported by this provider.'}.to_json)
+      body({:error => 'Valid email and site url must be provided.'}.to_json)
     end
-    @@resources << resource = Resource.new(:id => @@resources.size + 1,
-                            :heroku_id => json_body['heroku_id'],
-                            :plan => json_body.fetch('plan', 'test'),
-                            :region => json_body['region'],
-                            :callback_url => json_body['callback_url'],
-                            :options => json_body['options'])
-    status 201
-    body({
-      :id => resource.id,
-      :config => {"MYADDON_URL" => 'http://yourapp.com/user'},
-      :message => 'Dear customer, your addon is now provisioned!'
-    }.to_json)
   end
 
   # deprovision
   delete '/heroku/resources/:id' do
     show_request
     protected!
-    @@resources.delete(get_resource)
-    "ok"
+
+    resource = get_resource
+    user_id = ENV["YOTTAA_USER_ID"]
+    site_id = resource['id']
+
+    # try to delete site
+    STDOUT.puts "Deleting Yottaa site with id " + site_id
+    uri = URI.parse(YOTTAA_API_URL_ROOT + "/accounts/" + user_id + "/sites/" +site_id)
+    https = https_connection(uri)
+    req = Net::HTTP::Delete.new(uri, YOTTAA_CUSTOM_HEADER)
+    https.set_debug_output($stdout)
+    res = https.request(req)
+    result = JSON.parse(res.body)
+
+    if !result.has_key? 'error'
+      "ok"
+    else
+      body(result["error"].to_json)
+    end
+
   end
 
   # plan change
@@ -114,7 +194,7 @@ class App < Sinatra::Base
     show_request
     protected!
     resource = get_resource
-    resource.plan = json_body['plan']
+    plan = json_body['plan']
     {}.to_json
   end
 end
---
+++ consumer.rb
@@ -0,0 +1,42 @@
+require 'bundler'
+require 'net/http'
+require 'net/https'
+require 'uri'
+require 'json'
+
+Bundler.require
+
+STDOUT.sync = true
+
+# utility class for yottaa custom header
+class CaseSensitiveString < String
+  def downcase
+    self
+  end
+
+  def capitalize
+    self
+  end
+end
+
+STDOUT.puts "Retrieve list of sites you have registerd with Yottaa."
+uri = URI.parse("https://api-dev.yottaa.com/sites")
+
+https = Net::HTTP.new(uri.host, uri.port)
+https.use_ssl = true
+https.verify_mode = OpenSSL::SSL::VERIFY_NONE
+
+custom_header_key = CaseSensitiveString.new("YOTTAA-API-KEY")
+req = Net::HTTP::Get.new(uri, {custom_header_key =>ENV['YOTTAA_API_KEY']})
+req.set_form_data({"user_id" => ENV['YOTTAA_USER_ID']})
+https.set_debug_output($stdout)
+
+res = https.request(req)
+
+result = JSON.parse(res.body)
+
+if !result.has_key? 'error'
+  STDOUT.puts result.to_json
+else
+  STDOUT.puts result.to_json
+end
