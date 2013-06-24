cb9376503ebc64584e3bb4e89a3f12e73b1742ae
drq <yong.qu@yottaa.com>
2013-06-23 23:53:33 -0400

    Added support for provision with existing Yottaa account. Improved SSO page.

--- app.rb
+++ app.rb
@@ -22,8 +22,10 @@ end
 class App < Sinatra::Base
   use Rack::Session::Cookie, secret: ENV['SSO_SALT']
 
-  YOTTAA_CUSTOM_HEADER = {CaseSensitiveString.new("YOTTAA-API-KEY") =>'39e8b9b0b1000130d279123138106137'}
-  YOTTAA_API_URL_ROOT = "https://api-dev.yottaa.com/partners/51b0cd1bebe2bb3069000f0d"
+  YOTTAA_API_URL_ROOT = "https://api-dev.yottaa.com"
+
+  YOTTAA_CUSTOM_HEADER_PARTNER = {CaseSensitiveString.new("YOTTAA-API-KEY") =>'39e8b9b0b1000130d279123138106137'}
+  YOTTAA_API_URL_PARTNER = YOTTAA_API_URL_ROOT + "/partners/51b0cd1bebe2bb3069000f0d"
 
   helpers do
     def protected!
@@ -59,9 +61,9 @@ class App < Sinatra::Base
       site_id = params[:id]
       user_id = ENV["YOTTAA_USER_ID"]
       if ! user_id.nil?
-        uri = URI.parse(YOTTAA_API_URL_ROOT + "/accounts/" + user_id + "/sites/" + site_id)
+        uri = URI.parse(YOTTAA_API_URL_PARTNER + "/accounts/" + user_id + "/sites/" + site_id)
         https = https_connection(uri)
-        req = Net::HTTP::Get.new(uri, YOTTAA_CUSTOM_HEADER)
+        req = Net::HTTP::Get.new(uri, YOTTAA_CUSTOM_HEADER_PARTNER)
         https.set_debug_output($stdout)
 
         res = https.request(req)
@@ -126,20 +128,28 @@ class App < Sinatra::Base
   post '/heroku/resources' do
     show_request
     protected!
+
+    # Try to see if any option is provided by command line parameters
     options = json_body.fetch('options', {})
-    first_name = options['first_name'].nil? ? "" : options['first_name']
-    last_name = options['last_name'].nil? ? "" : options['last_name']
-    phone = options['phone'].nil? ? "" : options['phone']
-    email = options['email'].nil? ? json_body.fetch('heroku_id') : options['email']
-    # TODO: figure out the default custom domain name?
-    site = options['site'].nil? ? "" : options['site']
+    first_name = options.fetch('first_name','')
+    last_name = options.fetch('last_name','')
+    phone = options.fetch('phone','')
+    email = options.fetch('email', json_body.fetch('heroku_id'))
+    site = options.fetch('site','')
+
+    site_id = options.fetch('site_id','')
+    user_id = options.fetch('user_id','')
+    api_key = options.fetch('api_key','')
+
     plan = json_body.fetch("plan", "free")
+
+    # Check if both email and site are provided
     if !email.empty? && !site.empty?
       STDOUT.puts "Creating a new Yottaa account"
 
-      uri = URI.parse(YOTTAA_API_URL_ROOT + "/accounts")
+      uri = URI.parse(YOTTAA_API_URL_PARTNER + "/accounts")
       https = https_connection(uri)
-      req = Net::HTTP::Post.new(uri, YOTTAA_CUSTOM_HEADER)
+      req = Net::HTTP::Post.new(uri, YOTTAA_CUSTOM_HEADER_PARTNER)
       req.set_form_data({"first_name" => first_name, "last_name" => last_name, "email" => email, "site" => site, "phone" => phone, "plan" => plan})
       https.set_debug_output($stdout)
 
@@ -148,18 +158,51 @@ class App < Sinatra::Base
 
       if !result.has_key? 'error'
         status 201
+        resource_id = result["site_id"]
         body({
-                 :id => result["site_id"],
+                 :id => resource_id,
                  :config => {"YOTTAA_SITE_ID" => result["site_id"], "YOTTAA_USER_ID" => result["user_id"], "YOTTAA_API_KEY" => result["api_key"]},
                  :message => 'Dear ' + first_name + ' ' + last_name +', your Yottaa account is now provisioned!'
              }.to_json)
+=begin
+        # Try to figure out the domain name of the app which just installed this add-on
+        uri = URI.parse("https://api.heroku.com/vendor/apps/" + resource_id)
+        https = https_connection(uri)
+        HEROKU_CUSTOM_HEADER = {"Accept" =>'application/vnd.heroku+json; version=3'}
+        req = Net::HTTP::Get.new(uri, HEROKU_CUSTOM_HEADER)
+        req.basic_auth "yong.qu@yottaa.com", "y0ttaa1234"
+        https.set_debug_output($stdout)
+        res = https.request(req)
+        result = JSON.parse(res.body)
+        domain = result["domains"][0]
+        # Now update Yottaa account for the actual domain name???
+=end
       else
         status 422
         body(result['error'].to_json)
       end
     else
-      status 422
-      body({:error => 'Valid email and site url must be provided.'}.to_json)
+      if !site_id.empty? && !user_id.empty? && !api_key.empty?
+        # validate the combo
+        uri = URI.parse(YOTTAA_API_URL_ROOT + "/sites/" + site_id)
+        https = https_connection(uri)
+        req = Net::HTTP::Get.new(uri, {CaseSensitiveString.new("YOTTAA-API-KEY") =>api_key})
+        req.set_form_data({"user_id" => user_id})
+        https.set_debug_output($stdout)
+
+        res = https.request(req)
+        result = JSON.parse(res.body)
+        if !result.has_key? 'error'
+          body({
+                   :id => site_id,
+                   :config => {"YOTTAA_SITE_ID" => site_id, "YOTTAA_USER_ID" => user_id, "YOTTAA_API_KEY" => api_key},
+                   :message => 'Dear customer' +', your Yottaa account is now provisioned!'
+               }.to_json)
+         end
+      else
+        status 422
+        body({:error => 'Valid email and site url must be provided.'}.to_json)
+      end
     end
   end
 
@@ -174,9 +217,9 @@ class App < Sinatra::Base
 
     # try to delete site
     STDOUT.puts "Deleting Yottaa site with id " + site_id
-    uri = URI.parse(YOTTAA_API_URL_ROOT + "/accounts/" + user_id + "/sites/" +site_id)
+    uri = URI.parse(YOTTAA_API_URL_PARTNER + "/accounts/" + user_id + "/sites/" +site_id)
     https = https_connection(uri)
-    req = Net::HTTP::Delete.new(uri, YOTTAA_CUSTOM_HEADER)
+    req = Net::HTTP::Delete.new(uri, YOTTAA_CUSTOM_HEADER_PARTNER)
     https.set_debug_output($stdout)
     res = https.request(req)
     result = JSON.parse(res.body)
